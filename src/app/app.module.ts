aff3209a6718962697923c46626d4f538c597690
Dimas <redisotschek@gmail.com>
2022-05-18 03:18:30 +0400

    import ngzorro components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
 
 registerLocaleData(en);
 
@@ -34,6 +42,14 @@ registerLocaleData(en);
     NzButtonModule,
     NzIconModule,
     NzToolTipModule,
+    NzModalModule,
+    NzTableModule,
+    NzSpinModule,
+    NzIconModule,
+    NzAvatarModule,
+    NzEmptyModule,
+    NzBadgeModule,
+    NzPopoverModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
