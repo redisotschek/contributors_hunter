cc784432de09558b6eebb5d3c48704c4c793900d
Dimas <redisotschek@gmail.com>
2022-05-18 03:19:52 +0400

    create wrapper in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ApiService } from './services/api.service';
 import { OrganizationSearchService } from './services/organization-search.service';
 
 @Component({
@@ -8,21 +7,14 @@ import { OrganizationSearchService } from './services/organization-search.servic
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public searchString: string = '';
-  public privateToken: string = '';
-  public authToken: string = '';
+  public searchString: string = 'octokit';
 
   constructor(
     private organizationSearchService: OrganizationSearchService,
-    private apiService: ApiService,
   ) {}
 
   public search(): void {
+    console.log("search clicked");
     this.organizationSearchService.searchOrganizationRepositories(this.searchString);
-  }
-
-  public authenticate(): void {
-    const res: any = this.apiService.authenticate(this.privateToken);
-    console.log(res);
   }
 }
